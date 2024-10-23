b2c92ecf568a0bbbfcccd41e9ea856a222fd67d6
Joshua S. <jstal791@gmail.com>
2024-10-23 12:54:13 -0500

    Changed version code and name for playstore console upload
    SIGNING KEY ALIAS AND PASSWORD AND KEYSTORE PASSWORD ARE ALL THE SAME

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.orthodoxquotesapp.quoteapp"
         minSdk = 28 // default 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
